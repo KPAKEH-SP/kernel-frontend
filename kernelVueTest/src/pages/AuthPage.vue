9e09ffd7400d95631fe25b99e222e9277f306db8
Denis Emrih <den.emrikh@mail.ru>
2024-12-10 00:51:52 +0400

    change api

--- kernelVueTest/src/pages/AuthPage.vue
+++ kernelVueTest/src/pages/AuthPage.vue
@@ -22,7 +22,7 @@
             openedLoginPanel.value = true;
             return;
         }
-        axios.post("https://194.87.140.155:8080/api/auth/user-info", {
+        axios.post("/api/auth/user-info", {
             token: storageToken
         })
         .then(function(response) {
@@ -110,7 +110,7 @@
 
         regErrorShowed.value = false;
 
-        axios.post('http://194.87.140.155:8080/api/auth/registration', {
+        axios.post('/api/auth/registration', {
             username: regUsername.value,
             email: regEmail.value,
             password: regPassword.value
@@ -127,7 +127,7 @@
     }
 
     const login = () => {
-        axios.post("http://194.87.140.155:8080/api/auth/login", {
+        axios.post("/api/auth/login", {
             username: loginUsername.value,
             password: loginPassword.value
         })
