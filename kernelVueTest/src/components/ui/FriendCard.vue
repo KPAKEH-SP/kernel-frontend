b3cf018a1be039f30983847b16dbdd5cacb22986
Denis Emrih <den.emrikh@mail.ru>
2024-12-30 04:14:42 +0400

    -= changes =-

--- kernelVueTest/src/components/ui/FriendCard.vue
+++ kernelVueTest/src/components/ui/FriendCard.vue
@@ -33,7 +33,7 @@
         @click="toggleFlip">
     <div class="card">
       <div class="card-front">
-        <pixel-canvas data-colors="#2d3133, #365757, #003333" class="pixel-canvas"></pixel-canvas>
+        
         <div class="avatar">
           <img :src="getAvatar(props.username)" class="avatar-image" onerror="this.style.display='none';"/>
           <div v-if="showCircles" class="spinning-circles" id="circles">
@@ -49,7 +49,10 @@
         </div>
       </div>
       <div class="card-back">
-        <slot></slot>
+        <pixel-canvas data-colors="#2d3133, #365757, #003333" class="pixel-canvas"></pixel-canvas>
+        <div calss="back-slot">
+          <slot></slot>
+        </div>
       </div>
     </div>
   </div>
@@ -143,5 +146,12 @@
 
   .pixel-canvas {
     position: absolute;
+    pointer-events: none;
+    z-index: 1;
+  }
+
+  .back-slot {
+    position: inherit;
+    z-index: 30;
   }
 </style>
